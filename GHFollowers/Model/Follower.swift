1c72f4dd524674eb689452bace5e8efc301d35dd
Saurabh Jaiswal <sjsaurabhjais@gmail.com>
2024-11-28 13:13:22 +0530

    Fixed bug for navigationController setNavigationBarHidden and Added Network Manager and made a call for getFollowers array list.

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Saurabh Jaiswal on 27/11/24.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
