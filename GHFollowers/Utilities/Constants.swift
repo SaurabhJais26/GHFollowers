2d824079cc3a361022a5b2b23bad41d0d58b9e01
Saurabh Jaiswal <sjsaurabhjais@gmail.com>
2024-12-02 12:00:21 +0530

    Added UserInfoHeaderView and configure it to display it on the click of the cell.

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Saurabh Jaiswal on 01/12/24.
+//
+
+import Foundation
+
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+    
+}
