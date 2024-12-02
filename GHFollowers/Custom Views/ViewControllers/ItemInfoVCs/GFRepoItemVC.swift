6c4391b24f767192d0b9a8f71aeeb74fb13d2e00
Saurabh Jaiswal <sjsaurabhjais@gmail.com>
2024-12-02 15:44:00 +0530

    refactored the sub classes and added some views in it.

---
+++ GHFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFRepoItemVC.swift
@@ -0,0 +1,23 @@
+//
+//  GFRepoItemVC.swift
+//  GHFollowers
+//
+//  Created by Saurabh Jaiswal on 02/12/24.
+//
+
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "GitHub Profile")
+        
+    }
+}
