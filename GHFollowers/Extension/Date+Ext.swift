dab9a5c0b9e1da792f665c9ec5d84dcc0fc9f9e5
Saurabh Jaiswal <sjsaurabhjais@gmail.com>
2024-12-06 17:40:53 +0530

    new date formatter.

--- GHFollowers/Extension/Date+Ext.swift
+++ GHFollowers/Extension/Date+Ext.swift
@@ -9,9 +9,14 @@ import UIKit
 
 extension Date {
     
+//    func convertToMonthYearFormat() -> String {
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateFormat = "MMM yyyy"
+//        return dateFormatter.string(from: self)
+//    }
+    
     func convertToMonthYearFormat() -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MMM yyyy"
-        return dateFormatter.string(from: self)
+        return formatted(.dateTime.month().year())
     }
+    
 }
